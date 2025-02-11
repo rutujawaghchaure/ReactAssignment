d96e87e007a3bb759135e809b25afbf4c7e7e648
rutujawaghchaure <waghchaurerutuja3@gmail.com>
2025-02-11 17:31:26 +0530

    2303031087089 - Assignment 32

---
+++ Task32/Task32/src/App.jsx
@@ -0,0 +1,11 @@
+import Greet from "./components/Greet"
+
+function App() {
+  return (
+    <>
+    <Greet/>
+    </>
+  )
+}
+
+export default App
---
+++ Task32/Task32/src/components/Greet.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Greet=()=> {
+  return (
+    <>
+      <h1>This is Greet Function</h1>
+    </>
+  )
+}
+
+export default Greet
